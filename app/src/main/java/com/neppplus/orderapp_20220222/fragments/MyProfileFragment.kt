8500e4d2a3ce0f5d396a4106a370f8880758fde8
nepp.plus <nepp.plus@gmail.com>
2022-02-22 15:59:12 +0900

    프래그먼트로 돌아왔을때, 첨부한 닉네임 반영

--- app/src/main/java/com/neppplus/orderapp_20220222/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/neppplus/orderapp_20220222/fragments/MyProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.neppplus.orderapp_20220222.fragments
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -43,6 +44,18 @@ class MyProfileFragment : Fragment() {
     }
 
     fun setValues() {
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQ_CODE_NICKNAME) {
+           if (resultCode == Activity.RESULT_OK) {
+               val nickname = data!!.getStringExtra("nick")
+               txtNickname.text = nickname
+           }
+        }
 
     }
 
