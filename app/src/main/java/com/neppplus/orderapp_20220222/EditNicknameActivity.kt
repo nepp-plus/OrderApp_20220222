e35f0ae9735ca8cb017bb40a859581be6c3809f3
nepp.plus <nepp.plus@gmail.com>
2022-02-22 15:45:48 +0900

    닉네임 변경 화면 추가만

---
+++ app/src/main/java/com/neppplus/orderapp_20220222/EditNicknameActivity.kt
@@ -0,0 +1,22 @@
+package com.neppplus.orderapp_20220222
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNicknameActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_nickname)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+
+    }
+
+}
