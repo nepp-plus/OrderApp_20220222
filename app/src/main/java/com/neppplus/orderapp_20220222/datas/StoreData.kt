b8a0cc07690f0a72e656be246911d435e657f81d
nepp.plus <nepp.plus@gmail.com>
2022-02-22 14:42:16 +0900

    Serializable을 활용해서 클릭된 방을 첨부 > 화면 이동

--- app/src/main/java/com/neppplus/orderapp_20220222/datas/StoreData.kt
+++ app/src/main/java/com/neppplus/orderapp_20220222/datas/StoreData.kt
@@ -1,9 +1,11 @@
 package com.neppplus.orderapp_20220222.datas
 
+import java.io.Serializable
+
 class StoreData(
     val name: String,
     val rating: Double,
     val phoneNum: String,
     val logoImageURL: String,
-) {
+) : Serializable {
 }
