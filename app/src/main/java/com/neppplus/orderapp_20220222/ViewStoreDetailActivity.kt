375d1a8efc658fb9de8d6c938b75a5dddcc9c8c9
nepp.plus <nepp.plus@gmail.com>
2022-02-22 14:38:12 +0900

    리스트뷰의 이벤트 처리  - 단순 화면이동으로 먼저 구현

---
+++ app/src/main/java/com/neppplus/orderapp_20220222/ViewStoreDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.orderapp_20220222
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewStoreDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_store_detail)
+    }
+}
