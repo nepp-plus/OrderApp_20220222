708772e097b55eee78a6274a8e25e1646a1d01ba
nepp.plus <nepp.plus@gmail.com>
2022-02-22 09:48:51 +0900

    프래그먼트 클래스 1개 추가

---
+++ app/src/main/java/com/neppplus/orderapp_20220222/fragments/PizzaStoreListFragment.kt
@@ -0,0 +1,28 @@
+package com.neppplus.orderapp_20220222.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.neppplus.orderapp_20220222.R
+
+class PizzaStoreListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_pizza_store_list, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+//        피자 가게 리스트뷰의 동작 코드
+
+    }
+
+
+}
