d9ab8e6aeae0f94465d8719fc052802ab67666a3
nepp.plus <nepp.plus@gmail.com>
2022-02-22 14:47:00 +0900

    첨부해준 데이터 받아오기  - 멤버변수로 만들어서 활용

--- app/src/main/java/com/neppplus/orderapp_20220222/fragments/PizzaStoreListFragment.kt
+++ app/src/main/java/com/neppplus/orderapp_20220222/fragments/PizzaStoreListFragment.kt
@@ -2,6 +2,7 @@ package com.neppplus.orderapp_20220222.fragments
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
